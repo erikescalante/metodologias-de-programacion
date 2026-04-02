6c04e694af5cb46984f87b2281df6cb563064b1e
erik <escalanteerik05@gmail.com>
2026-04-02 19:47:34 -0300

    2nd commit. Practica 2, implementada la clase conjunto utilizando arbol avl (pobremente)

--- Practica2/Practica2/Conjunto.cs
+++ Practica2/Practica2/Conjunto.cs
@@ -1,6 +1,24 @@
+using System;
+using Practica2.Interfaces;
+
 namespace Practica2;
 
-public class Conjunto
+public class Conjunto(Comparable elemento) : Coleccionable
 {
+    //variable
+    private readonly ArbolAvl _arbolito = new(elemento);
     
+    //properties
+    
+    
+    //methods
+    public int Cuantos()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Comparable Minimo() => _arbolito.Minimo;
+    public Comparable Maximo() => _arbolito.Maximo;
+    public void Agregar(Comparable elemento) => _arbolito.Agregar(elemento);
+    public bool Contiene(Comparable elemento) => _arbolito.Incluido(elemento);
 }
--- Practica2/Practica2/obj/Debug/net10.0/Practica2.AssemblyInfo.cs
+++ Practica2/Practica2/obj/Debug/net10.0/Practica2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d7f27d3cd5487a2322d8b8b934a34510146797")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
